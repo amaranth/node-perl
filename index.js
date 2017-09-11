54edffe8565b255c243051cbe231880edc5bcb0a
bstriker <strikeforcezero@gmail.com>
2017-09-11 02:14:57 -0400

    Add prototype to specify lib directories

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+var path = require('path');
+var fs = require('fs');
 var P = require('./build/Release/perl.node');
 P.InitPerl();
+P.Perl.prototype.lib = function (libDir) {
+    if (!libDir || libDir.trim().length === 0) {
+        throw new Error("This is not a valid lib directory : " + libDir);
+    }
+    libDir = path.resolve(process.cwd(), libDir);
+    if (!fs.existsSync(libDir)) {
+        throw new Error("Requested lib directory does not exist : " + libDir);
+    }
+    return this.evaluate('use lib qw(' + libDir + ')');
+};
 P.Perl.prototype.use = function (name) {
     if (!name.match(/^[A-Za-z0-9_:]+$/)) {
         throw new Error("This is not a valid class name : " + name);
