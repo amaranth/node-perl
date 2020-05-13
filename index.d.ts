cb04df26034bc54aba7845333d44ebcb9b7ec107
bstriker <strikeforcezero@gmail.com>
2020-05-13 07:38:36 -0400

    WIP

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,9 @@ declare class Perl {
     evaluate(input: string): unknown;
     use(lib: string): void;
     destroy(): void;
+    getClass(className: string): unknown;
+    static blessed(obj: object): string;
+    call(...args: any[]): unknown;
 }
 
 export = Perl;
